78f364b8008efd07bd8705b66b333643b6a9eda1
Pranita <fulawade.pranita@gmail.com>
2022-03-14 16:09:46 +0530

    added base class

---
+++ src/test/java/BDD/Cucumber/Base.java
@@ -0,0 +1,16 @@
+package BDD.Cucumber;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Base {
+	WebDriver driver;
+	
+	public void beforeClass()
+	{
+		System.setProperty("webdriver.edge.drive","E:\\CucumberTutorial\\ProjectX\\GitDemo\\src\\test\\java\\resources\\msedgedriver.exe");
+		driver=new EdgeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
